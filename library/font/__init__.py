b34c069df828cbf1c050abb6f7380b78b4bf82bb
Peter Albu <pecasdev@gmail.com>
2024-07-23 18:25:55 -0400

    Add "-" to font
    This replaces whatever godforsaken character I had in the bottom right before

--- library/font/__init__.py
+++ library/font/__init__.py
@@ -1,7 +1,4 @@
-from engine import Engine
 import font.persist as font_persist
-from sprite import Sprite
-
 
 class Font:
     known_fonts = []
@@ -10,7 +7,7 @@ class Font:
     available_characters = (
         [chr(ord("A") + i) for i in range(26)]
         + [str(i) for i in range(10)]
-        + [c for c in "><!?:*"]
+        + [c for c in "><!?:-"]
     )
 
     @staticmethod
@@ -31,19 +28,29 @@ class Font:
         font = font_persist.load(filename)
         Font.known_fonts.append(font)
 
-    def __init__(self, name: str, character_sprites: dict[str, Sprite]):
+    def __init__(self, name: str, character_sprites):
         self.name = name
         self.character_sprites = character_sprites
         self.char_w = character_sprites["A"].width
         self.char_h = character_sprites["A"].height
 
     def draw_text(self, x: int, y: int, text: str):
-        for index, char in enumerate(text):
-            self.__draw_character(x + index * (self.char_w + 1), y, char)
+        lines = text.split("\n")
+        for y_offset, line in enumerate(lines):
+            for x_offset, char in enumerate(line):
+                self.__draw_character(
+                    x + x_offset * (self.char_w + 1),
+                    y + y_offset * (self.char_h + 2),
+                    char,
+                )
 
     def __draw_character(self, x: int, y: int, character: str):
         if character == " ":
             return
 
+        upper_character = character.upper()
+        if upper_character not in self.character_sprites:
+            raise RuntimeError(f"Font has not been defined for '{upper_character}'")
+
         sprite = self.character_sprites[character.upper()]
-        sprite.draw(x, y)
+        sprite.blit(x, y)
