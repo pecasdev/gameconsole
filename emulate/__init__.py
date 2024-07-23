668cb98c152ee98ff35db4f7e31687d406f17daa
Peter Albu <pecasdev@gmail.com>
2024-07-23 18:10:01 -0400

    Move font importing for emulation

--- emulate/__init__.py
+++ emulate/__init__.py
@@ -5,7 +5,7 @@ from .props.arrange_config import OLED, CASE
 from .emulator import Emulator
 from screen import Screen
 from .pygame_screen_adaptor import PygameScreenAdaptor
-
+from font import Font
 
 def create_emulator(program_main):
     pygame_driver = PygameDriver()
@@ -13,6 +13,13 @@ def create_emulator(program_main):
 
     engine_psa = PygameScreenAdaptor(OLED["WIDTH"], OLED["HEIGHT"], pygame_driver)
     Engine.set_screen(Screen(engine_psa))
+    
+    # import default fonts
+    Font.import_font("library/font/default_fonts/small.font")
+    
+    # run injection setup
     program_main()
+    
+    # return emulator
     emulator = Emulator(engine_psa, pygame_driver)
     return emulator
