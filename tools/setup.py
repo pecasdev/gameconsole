12b2d626b50187c5cd0a955975ae513d2cd02f95
Peter Albu <pecasdev@gmail.com>
2024-07-29 19:04:54 -0400

    Fix tools module

--- tools/setup.py
+++ tools/setup.py
@@ -5,5 +5,6 @@ setup(
     version="1.0",
     description="tools for programming gameconsole games",
     author="Peter Albu",
-    author_email="pecasdev@gmail.com"
+    author_email="pecasdev@gmail.com",
+    packages=["flash", "font_convert", "sprite_convert"]
 )
