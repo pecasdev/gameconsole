199c880d24be5a2ed9883cfe29c32837183736df
Peter Albu <pecasdev@gmail.com>
2024-07-29 19:04:41 -0400

    Fix import issues

--- __init__.py
+++ __init__.py
@@ -1,5 +1,12 @@
-from inject import main
-from emulate import create_emulator
-
 if __name__ == "__main__":
+    # idk if this is illegal but i can't get imports to work properly, i'm so tired
+    import os
+    import sys
+
+    gameconsole_dir = os.path.dirname(__file__)
+    sys.path.insert(0, os.path.join(gameconsole_dir, "library"))
+
+    from inject import main
+    from emulate import create_emulator
+
     create_emulator(main).runloop()
