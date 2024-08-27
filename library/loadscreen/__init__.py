a72883259637701059c02ea3eb651a91e69641e9
Peter Albu <pecasdev@gmail.com>
2024-08-27 12:59:35 -0400

    Make loadscreen text fit on screen

--- library/loadscreen/__init__.py
+++ library/loadscreen/__init__.py
@@ -3,6 +3,6 @@ from engine import Engine
 
 def draw_loadscreen(title: str):
     Engine.screen.clear()
-    Engine.screen.draw_text(10, 10, f"NOW LOADING {title}")
-    Engine.screen.draw_text(10, 20, f"THIS MIGHT TAKE 10 SECONDS")
+    Engine.screen.draw_text(5, 10, f"NOW LOADING {title}")
+    Engine.screen.draw_text(5, 20, f"THIS CAN TAKE\nUP TO 10 SECONDS")
     Engine.screen.flush()
