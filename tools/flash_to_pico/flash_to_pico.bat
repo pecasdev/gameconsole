a2821e8a62bb21689df19cb01e6ad261f438079d
Peter Albu <pecasdev@gmail.com>
2024-08-27 10:45:07 -0400

    Reorganize flash_to_pico lines alphabetically

--- tools/flash_to_pico/flash_to_pico.bat
+++ tools/flash_to_pico/flash_to_pico.bat
@@ -7,11 +7,11 @@ mpremote ^
 mip install sdcard + ^
 mip install ssd1306 + ^
 cp -r library : + ^
+cp hardware/copy_injection_from_sd.py : + ^
 cp hardware/engine_driver.py : + ^
 cp hardware/hardware_button_driver.py : + ^
 cp hardware/hardware_frame_buffer_adaptor.py : + ^
-cp hardware/oled_screen_adaptor.py : + ^
-cp hardware/copy_injection_from_sd.py : + ^
-cp hardware/mount_sd.py : + ^
 cp hardware/inject_select_menu.py : + ^
-cp hardware/main2.py :main2.py
+cp hardware/main2.py :main2.py + ^
+cp hardware/mount_sd.py : + ^
+cp hardware/oled_screen_adaptor.py :
